a529df68f9557b12ae193c723b041a351010cb25
Luana Uchoa <uchoa.m.luana@gmail.com>
2017-09-21 01:46:48 -0300

    lru nao finalizado

--- Paginacao/src/simulador/LRU.java
+++ Paginacao/src/simulador/LRU.java
@@ -2,8 +2,10 @@ package simulador;
 
 import java.util.LinkedList;
 
-public class LRU extends AlgoritmoDeSubstituicao <QuadroLRU> {
-	
+public class LRU extends AlgoritmoDeSubstituicao<QuadroLRU> {
+
+	private int insere = 0;
+
 	public LRU(int numeroDeQuadros) {
 		super(numeroDeQuadros);
 		this.quadros = new LinkedList<QuadroLRU>();
@@ -12,33 +14,52 @@ public class LRU extends AlgoritmoDeSubstituicao <QuadroLRU> {
 	@Override
 	public void inserir(String valor) {
 
-		QuadroLRU quadro = new QuadroLRU();
-		quadro.setValor(valor);
+		QuadroLRU quadro = new QuadroLRU(valor);
+		int maior = numeroDeQuadros - 1;
 
-		// Se NÃO contem na cache o valor
-		if (!quadros.contains(valor)) {
+		// Verifica se o quadro esta na cache
+		if (!quadros.contains(quadro)) {//VERIFICAR PQ NUNCA ENTRA NO ELSE
 
 			// Se ainda tem espaço na cache
 			if (quadros.size() < numeroDeQuadros) {
 				quadros.add(quadro);
-				// Se contem na cache o valor
+				for (int i = 1; i < quadros.size(); i++) {
+					quadros.get(i-1).setIdade(quadros.size()-i);
+				}
+				
 			} else {
 
+				for (int i = 0; i < numeroDeQuadros; i++) {
+					if (quadros.get(i).getIdade() > quadros.get(maior).getIdade()) {
+						maior = i;
+					}
+				}
+				quadros.set(maior, quadro);
 			}
 
 			cacheMiss++;
 
+		// Se a cache contem o valor
 		} else {
-			for (int i = 0; i < quadros.size(); i++) {
-				if(quadros.get(i).getValor().contains(valor)) 
-					quadros.get(i).setZero();
-				else 
-					quadros.get(i).incIdade();					
-				
-			}
 			cacheHit++;
-			
+			for (int i = 0; i < quadros.size(); i++) {
+				if (quadros.get(i).getValor() == valor)
+					quadros.get(i).setZero();
+				else
+					quadros.get(i).incIdade();
+
+			}
+
 		}
+
 	}
 
+	@Override
+	public void imprimirQuadros() {
+		System.out.print("Quadros:  ");
+		for (int i = 0; i < quadros.size(); i++) {
+			System.out.print("[" + quadros.get(i).getValor() + " |" + quadros.get(i).getIdade() + " ]" + " ");
+		}
+		System.out.println();
+	}
 }
